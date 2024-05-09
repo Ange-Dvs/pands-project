19e3b58909b2b17340d9689edb6739d8d91d787d
Angela Davis <angela__davis@outlook.com>
2024-05-09 23:48:07 +0100

    Error handling adjusted for functions using loops to avoid repetition of error messages.  Read clean up started to add refactored code and remove unneccessary cells. Refactoring of correlation calculation started but further work needed.

--- analysis.py
+++ analysis.py
@@ -59,59 +59,53 @@ def create_histograms(PNG_filenames, variable_to_plot): # creating a function wh
                 plt.savefig(PNG_filenames, bbox_inches='tight') # saving scatter plotas PNG file with bbox_inches set to tight to avoid any text or info being cut from the figure
                 plt.clf() # clears the current plot to allow a fresh plot for the next histogram to be created
 
-                print(f'Histogram {PNG_filenames} created.\n') # printing the confirmation for a histograms creation 
+                print(f'{PNG_filenames} created.\n') # printing the confirmation for a histograms creation 
 
         except FileExistsError: # If the PNG file is found to already exist when calling check_png_file_exists() function this path is followed
                 print(f'Error: Filename {PNG_filenames} already exists in folder. \n') # printing the error that the histogram already exists
 
 def create_scatter_all_variables(x_variable, y_variable, PNG_filenames):
-        # Creating a scatter plot showing each pair of variables 
-    # Creating the .png file for the scatter plot containing the sub-plots of the each variable compared 
-        try: # First this path is attempted to be executed
-                check_png_file_exists(PNG_filenames) # the function earlier created to check if the png file exists is triggered. If the file does not exist the code will continue to the next line. If the file does already exist the FileExistsError will be thrown and the code will jump to the except condition.
-                if i == 1 or i == 6 or i == 11 or i == 16:
-                    plt.subplot(4,4,i)
-                    plt.yticks([])
-                    plt.xticks([])
-                    plt.plot()
-                    if i == 1:
+        if i == 1 or i == 6 or i == 11 or i == 16:
+                plt.subplot(4,4,i)
+                plt.yticks([])
+                plt.xticks([])
+                plt.plot()
+                if i == 1:
                         plt.figtext(.16, .82, 'Sepal\nLength', ha='center', fontsize=22, fontstyle='italic')
-                    elif i == 6:
+                elif i == 6:
                         plt.figtext(.39, .59, 'Sepal\nWidth', ha='center', fontsize=22, fontstyle='italic')
-                    elif i == 11:
+                elif i == 11:
                         plt.figtext(.63, .35, 'Petal\nLength', ha='center', fontsize=22, fontstyle='italic')
-                    elif i == 16:
+                elif i == 16:
                         plt.figtext(.86, .12, 'Petal\nWidth', ha='center', fontsize=22, fontstyle='italic')
                         plt.tight_layout()
                         plt.suptitle('All variables', y=1.08, fontsize=32)
                         plt.figlegend(loc = "upper center", ncols = 3, bbox_to_anchor=(0.5, 1.04), fontsize=14, labels=['Setosa', 'Versicolor', 'Virginica']) # creating one legend for the subplots and setting it's location to the top center for the plot. Manually setting the labels to avoid it being set repeatly in the loop
                         plt.savefig(PNG_filenames, bbox_inches='tight') # saving scatter plot as PNG file with bbox_inches set to tight to adjust to fit the whole figure
-                else:
-                    plt.subplot(4,4,i)
-                    plt.scatter(setosa_df[x_variable], setosa_df[y_variable], color='violet')
-                    plt.scatter(versicolor_df[x_variable], versicolor_df[y_variable], color='navy')
-                    plt.scatter(virginica_df[x_variable], virginica_df[y_variable], color='orange')
-                    if i == 3 or i == 7 or i == 8 or i == 9 or i == 10 or i == 14:
+                        print(f'{PNG_filenames} created.\n') # printing the confirmation for a histograms creation
+        else:
+                plt.subplot(4,4,i)
+                plt.scatter(setosa_df[x_variable], setosa_df[y_variable], color='violet')
+                plt.scatter(versicolor_df[x_variable], versicolor_df[y_variable], color='navy')
+                plt.scatter(virginica_df[x_variable], virginica_df[y_variable], color='orange')
+                if i == 3 or i == 7 or i == 8 or i == 9 or i == 10 or i == 14:
                         plt.yticks([])
                         plt.xticks([])
-                    elif i == 2:
+                elif i == 2:
                         plt.yticks([])
                         plt.tick_params(axis='x', direction='out', top=True, labeltop=True, bottom=False, labelbottom=False)
-                    elif i == 4: 
+                elif i == 4: 
                         plt.tick_params(axis='y', direction='out', right=True, labelright=True, left=False, labelleft=False)
                         plt.tick_params(axis='x', direction='out', top=True, labeltop=True, bottom=False, labelbottom=False)
-                    elif i == 5:
+                elif i == 5:
                         plt.xticks([])
-                    elif i == 12:
+                elif i == 12:
                         plt.tick_params(axis='y', direction='out', right=True, labelright=True, left=False, labelleft=False)
                         plt.xticks([])
-                    elif i == 15:
+                elif i == 15:
                         plt.yticks([])
 
-        except FileExistsError: # If the PNG file is found to already exist when calling check_png_file_exists() function this path is followed
-                print(f'Error: Filename {PNG_filenames} already exists in folder.\n') # printing the error that the histogram already exists
-
-def setting_axis_limits(): # function to overwrite the range automatically populated for plots and instead use set values for ranges for the x and y axis 
+def setting_axis_limits(): # function to overwrite the range automatically populated for plots and instead use set values for ranges for y axis 
     ymin = 0
     if i == 1:
         ymax = 19.5
@@ -123,41 +117,38 @@ def setting_axis_limits(): # function to overwrite the range automatically popul
         ymax = 42
     plt.ylim(ymin, ymax) # setting min and max values for the y axis 
 
-def set_bins_width(histogram_to_plot):
+def set_bins_width(variable_to_plot):
         # Compute the bin edges based on the overall range of the variables
-        min_width = min(setosa_df[histogram_to_plot].min(), versicolor_df[histogram_to_plot].min(), virginica_df[histogram_to_plot].min())
-        max_width = max(setosa_df[histogram_to_plot].max(), versicolor_df[histogram_to_plot].max(), virginica_df[histogram_to_plot].max())
+        min_width = min(setosa_df[variable_to_plot].min(), versicolor_df[variable_to_plot].min(), virginica_df[variable_to_plot].min())
+        max_width = max(setosa_df[variable_to_plot].max(), versicolor_df[variable_to_plot].max(), virginica_df[variable_to_plot].max())
         bin_edges = np.linspace(min_width, max_width, 11)  # 10 bins with equal width
         return bin_edges
 
-def create_histogram_per_classes(s, histogram_to_plot, PNG_filenames):
-        try: # First this path is attempted to be executed
-                check_png_file_exists(PNG_filenames) # the function earlier created to check if the png file exists is triggered. If the file does not exist the code will continue to the next line. If the file does already exist the FileExistsError will be thrown and the code will jump to the except condition.
-                bin_edges = set_bins_width(histogram_to_plot)
-                plt.subplot(4, 3, s)  # Creating a subplot for sepal length vs sepal width
-                setting_axis_limits()
-                plt.hist(setosa_df[histogram_to_plot], bins=bin_edges, color='violet')
-                plt.yticks([])
-                plt.title('--------------------', fontsize = 20)
+def create_histogram_per_classes(s, variable_to_plot, PNG_filenames):
+        bin_edges = set_bins_width(variable_to_plot)
+        plt.subplot(4, 3, s)  # Creating a subplot for sepal length vs sepal width
+        setting_axis_limits()
+        plt.hist(setosa_df[variable_to_plot], bins=bin_edges, color='violet')
+        plt.yticks([])
+        plt.title('--------------------', fontsize = 20)
 
-                plt.subplot(4, 3, s+1)  # Creating a subplot for sepal length vs sepal width
-                setting_axis_limits()
-                plt.hist(versicolor_df[histogram_to_plot], bins=bin_edges, color='navy')
-                plt.yticks([])
-                plt.title('Sepal Length (cm)', fontsize = 20)
-                
-                plt.subplot(4, 3, s+2)  # Creating a subplot for sepal length vs petal width  
-                setting_axis_limits()
-                plt.hist(virginica_df[histogram_to_plot], bins=bin_edges, color='orange')
-                plt.tick_params(axis='y', direction='out', right=True, labelright=True, left=False, labelleft=False)
-                plt.title('--------------------', fontsize = 20)
-                if s+2 == 12:
-                        plt.tight_layout()
-                        plt.suptitle('Histograms per class', y=1.08, fontsize=32)
-                        plt.figlegend(loc = "upper center", ncols = 3, bbox_to_anchor=(0.5, 1.04), fontsize=14, labels=['Setosa', 'Versicolor', 'Virginica']) # creating one legend for the subplots and setting it's location to the top center for the plot
-                        plt.savefig(PNG_filenames, bbox_inches='tight') # saving scatter plot as PNG file with bbox_inches set to tight to adjust the title
-        except FileExistsError: # If the PNG file is found to already exist when calling check_png_file_exists() function this path is followed
-                print(f'Error: Filename {PNG_filenames} already exists in folder.\n') # printing the error that the histogram already exists
+        plt.subplot(4, 3, s+1)  # Creating a subplot for sepal length vs sepal width
+        setting_axis_limits()
+        plt.hist(versicolor_df[variable_to_plot], bins=bin_edges, color='navy')
+        plt.yticks([])
+        plt.title(variable_to_plot, fontsize = 20)
+        
+        plt.subplot(4, 3, s+2)  # Creating a subplot for sepal length vs petal width  
+        setting_axis_limits()
+        plt.hist(virginica_df[variable_to_plot], bins=bin_edges, color='orange')
+        plt.tick_params(axis='y', direction='out', right=True, labelright=True, left=False, labelleft=False)
+        plt.title('--------------------', fontsize = 20)
+        if s+2 == 12:
+                plt.tight_layout()
+                plt.suptitle('Histograms per class', y=1.08, fontsize=32)
+                plt.figlegend(loc = "upper center", ncols = 3, bbox_to_anchor=(0.5, 1.04), fontsize=14, labels=['Setosa', 'Versicolor', 'Virginica']) # creating one legend for the subplots and setting it's location to the top center for the plot
+                plt.savefig(PNG_filenames, bbox_inches='tight') # saving scatter plot as PNG file with bbox_inches set to tight to adjust the title
+                print(f'{PNG_filenames} created.\n') # printing the confirmation for a histograms creation
 
 # fetching data from csv file
 df = pd.read_csv('iris_data.csv')
@@ -171,7 +162,7 @@ virginica_df = df[df['class'] == 'Iris-virginica']
 num_values = df.describe()
 intro = (f'The IRIS dataset, created in 1936, is a popular dataset commonly used for exploring data analysis and data visualisation.\n\nSpecies:\n\tThe dataset consists of measurements for 3 different classes (Setosa, Versicolor and Virginica) of Iris flowers.\nThere are 50 entries per class detailed in the dataset.\nAs the classes variable are plain text, the data type string will be applicable here.\n\nFour characteristics of the flowers were tracked including sepal length (cm), sepal width (cm), petal length (cm) and petal width (cm)\nThese four variables are numeric values and looking at the raw data we can see decimal places are present.\nWith this, the data type used for this variables will be float.\n\n')
 
-# Define dictionary of variables to plot for histograms and filenames for histogram pngs
+# Define dictionary of variables to plot for histograms and filenames for the individual histogram pngs
 variables_and_filenames = {
     'sepal length (cm)': '1_sepal_length_hist.png',
     'sepal width (cm)': '2_sepal_width_hist.png',
@@ -191,17 +182,23 @@ PNG_filenames = '5_all_variables_scatter.png'
 # Iterate over variables and filenames
 plt.figure(figsize=(9, 9)) # figure size is set to avoid the plot being overloaded
 for i, (x_variable, y_variable) in enumerate(x_variables_y_variables, start=1):
-       create_scatter_all_variables(x_variable, y_variable, PNG_filenames)
+        try: 
+                check_png_file_exists(PNG_filenames) # the function earlier created to check if the png file exists is triggered. If the file does not exist the code will continue to the next line. If the file does already exist the FileExistsError will be thrown and the code will jump to the except condition.   
+                create_scatter_all_variables(x_variable, y_variable, PNG_filenames)   
 
-print(f'Scatter plot {PNG_filenames} created.\n') # printing the confirmation for a histograms creation 
+        except FileExistsError: # If the PNG file is found to already exist when calling check_png_file_exists() function this path is followed
+                print(f'Error: Filename {PNG_filenames} already exists in folder.\n') # printing the error that the histogram already exists
+                break
 
-histograms_to_plot = ['sepal length (cm)', 'sepal width (cm)', 'petal length (cm)', 'petal width (cm)']
 s=1
 PNG_filenames = '6_classes_histogram.png'
 plt.figure(figsize=(9, 12)) # figure size is set to avoid the plot being overloaded
-for i, (histogram_to_plot) in enumerate(histograms_to_plot, start=1):
-    create_histogram_per_classes(s, histogram_to_plot, PNG_filenames)
-    s += 3
-
-print(f'Multiplot of histograms {PNG_filenames} created.\n') # printing the confirmation for a histograms creation 
-
+for i, (variable_to_plot) in enumerate(variables_and_filenames, start=1):
+        try:
+                check_png_file_exists(PNG_filenames) # the function earlier created to check if the png file exists is triggered. If the file does not exist the code will continue to the next line. If the file does already exist the FileExistsError will be thrown and the code will jump to the except condition.
+                create_histogram_per_classes(s, variable_to_plot, PNG_filenames)
+                s += 3
+                
+        except FileExistsError: # If the PNG file is found to already exist when calling check_png_file_exists() function this path is followed
+                print(f'Error: Filename {PNG_filenames} already exists in folder.\n') # printing the error that the histogram already exists
+                break
